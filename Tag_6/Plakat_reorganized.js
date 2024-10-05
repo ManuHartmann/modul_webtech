23ea51ed78301e87ed5dbc120a9a154e164d520d
ManuHartmann <manuela.hartmann@stud.sfgz.ch>
2024-10-05 12:12:17 +0200

    5.Okt_präsentation

--- Tag_6/Plakat_reorganized.js
+++ Tag_6/Plakat_reorganized.js
@@ -1,9 +1,43 @@
+// Cursor
+// var xp = 0,
+//   mouseX = 0;
+// var yp = 0,
+//   mouseY = 0;
+// var xpDot = 0,
+//   mouseX = 0;
+// var ypDot = 0,
+//   mouseY = 0;
+
+// const cursorFollower = document.querySelector(".cursorFollower");
+// const cursorFollowerDot = document.querySelector(".cursorFollowerDot");
+
+// document.addEventListener("mousemove", (e) => {
+//   mouseX = e.pageX;
+//   mouseY = e.pageY;
+// });
+
+// setInterval(function () {
+//   xp += (mouseX - xp) / 15;
+//   yp += (mouseY - yp) / 15;
+
+//   cursorFollower.style.left = xp + "px";
+//   cursorFollower.style.top = yp + "px";
+// }, 20);
+
+// setInterval(function () {
+//   xpDot += (mouseX - xpDot) / 25;
+//   ypDot += (mouseY - ypDot) / 25;
+
+//   cursorFollowerDot.style.left = xpDot + "px";
+//   cursorFollowerDot.style.top = ypDot + "px";
+// }, 20);
+
 const title = document.getElementById('title')
 
 // Initialize the Typewriter effect for the title
 const titleTypewriter = new Typewriter(title, {
     loop: false,
-    delay: 20,
+    delay: 50,
     deleteSpeed: 25,
     cursor: "|"
 });
@@ -15,13 +49,13 @@ const openingHours = document.getElementById('opening-hours')
 
 const openingHoursTypewriter = new Typewriter(openingHours, {
     loop: false,
-    delay: 20,
+    delay: 50,
     deleteSpeed: 25,
     autoStart: true,
     cursor: "|"
 });
 
-openingHoursTypewriter.typeString("10. Januar bis 30. April 1960<br />Offen: Montag 14–18, 20–22<br />Dienstag–Freitag 10–12, 14–18, 20–22<br />Samstag–Sonntag 10–12, 14–17").callFunction(() => {
+openingHoursTypewriter.typeString("10. Januar bis 30. April 1960<br /><br />Offen: Montag 14–18, 20–22<br />Dienstag–Freitag 10–12, 14–18, 20–22<br />Samstag–Sonntag 10–12, 14–17").callFunction(() => {
     // hide the cursor after typing is done
     let cursor2 = document.querySelector('.Typewriter__cursor');
     if (cursor2) {
